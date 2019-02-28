7beeab55d8f8b8dba011e2a449249932884e30a3
Abdu rahman <abdurahmant7@gmail.com>
2019-02-28 13:34:16 +0530

    navbar design commit

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoos=require('mongoose');
+var hbs=require('hbs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+//hbs.registerPartial(path.join(__dirname,'/views/partials'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
